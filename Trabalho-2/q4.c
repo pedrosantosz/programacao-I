d44493afde15cc1727dcf09b99146f2e16e3c564
Pedro Santos <89491903+pedrosantosz@users.noreply.github.com>
2022-11-16 01:08:15 -0300

    Update q4.c

--- Trabalho-2/q4.c
+++ Trabalho-2/q4.c
@@ -5,18 +5,18 @@ int main() {
 	printf("Informe o saldo da sua conta corrente e da sua poupanca respectivamente: ");
 	scanf("%f %f", &contaCorrente, &poupanca);
 	
-	if (contaCorrente >= 1000 || poupanca >= 1000) {
+	if (contaCorrente > 1000 || poupanca > 1000) {
 		if (contaCorrente > poupanca)
-			if ((contaCorrente - 1000) >= 1000)
+			if (contaCorrente * 2 > poupanca * 3)
 				limite = contaCorrente * 2;
 			else
 				limite = poupanca * 3;
 		else
-			if ((poupanca - 1000)>= 1000)
+			if (poupanca * 2 > contaCorrente * 3)
 				limite = poupanca * 2;
 			else
 				limite = contaCorrente * 3;
-		printf("%.2f", limite);
+		printf("Limite da conta especial: R$ %.2f", limite);
 	} else
 		printf("Sem conta especial.");
 }
